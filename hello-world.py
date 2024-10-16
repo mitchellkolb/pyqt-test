90f536e55d43357c17dc6ed6c48f99d712b93d71
mitchellkolb <mkolb321@gmail.com>
2024-10-15 22:55:49 -0700

    Downloaded the library and tested first program and it works. Yay

---
+++ hello-world.py
@@ -0,0 +1,31 @@
+
+import sys, random
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo"]
+
+        self.button = QtWidgets.QPushButton("Click Me!")
+        self.text = QtWidgets.QLabel("Hello Worlds", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(400, 300)
+    widget.show()
+
+    sys.exit(app.exec())
